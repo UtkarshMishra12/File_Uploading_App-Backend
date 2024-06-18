7dd803d2db896111987905f56873ea72555e83f7
Utkarsh Mishra <nasautkarsh@gmail.com>
2024-06-18 22:13:57 +0530

    Added logic for sendimg mail on Uploading File (i.e To user)
    Signed-off-by: Utkarsh Mishra <nasautkarsh@gmail.com>

--- models/File.js
+++ models/File.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 
 
 const fileSchema = new mongoose.Schema({
@@ -16,5 +17,32 @@ const fileSchema = new mongoose.Schema({
         type:String,
     }
 });
+
+const connectTransporter = require("../config/nodemailer");
+
+//post middleware
+fileSchema.post("save", async function(doc){
+    try{
+        console.log("DOC->", doc);
+
+        //TRANSPORTER
+        let transporter = connectTransporter();
+
+        //send mail
+        let info = await transporter.sendMail({
+            from: `Utkarsh Mishra`,
+            to: doc.email,
+            subject: "New File Uploaded on Cloudinary by FileUploader Project",
+            html: `<h2>Hello ${doc.name} </h2> 
+            <p>File has been successfully Uploaded View here: <a href="${doc.imageUrl}">${doc.imageUrl}</a></p>`,
+        });
+
+        console.log("INFO->",info); 
+    }
+    catch(error){
+        console.error(error);
+    }
+})
+
 
 module.exports = mongoose.model("File", fileSchema);
